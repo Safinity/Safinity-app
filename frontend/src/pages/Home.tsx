a37d005dd88015105b7bf7be00f1af3a82202d1e
beatriz-castro <beatrizcastro@ua.pt>
2026-01-13 23:41:26 +0000

    feat: add HomeCard component and integrate it into Home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,45 @@
 import '../theme/theme.css';
+import styled from 'styled-components';
 
 import Navbar from '../components/Navbar';
+import HomeCard from '../components/HomeCard';
+import imgCard from '../assets/image-home-card.png';
+
+const Page = styled.div`
+  min-height: 100vh;
+  background-color: var(--background);
+`;
+
+const Content = styled.div`
+  padding: 64px var(--spacing-margem-lateral);
+
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 64px;
+`;
 
 export default function Home() {
-  return <Navbar userName="Jorge" avatarUrl="/Ellipse.png" />;
+  return (
+    <Page>
+      <Navbar userName="Jorge" avatarUrl="/Ellipse.png" />
+
+      <Content>
+        <HomeCard
+          title="Current events"
+          description="See here the list of events you are associated with."
+          image={imgCard}
+          buttonLabel="See events"
+          onClick={() => console.log('See events')}
+        />
+
+        <HomeCard
+          title="New event"
+          description="Create a new event and manage all its information here."
+          image={imgCard}
+          buttonLabel="Add new event"
+          onClick={() => console.log('Add new event')}
+        />
+      </Content>
+    </Page>
+  );
 }

bde06e94ef1f5b524f8e9af88724d8cba460a0fc
beatriz-castro <beatrizcastro@ua.pt>
2026-01-13 22:54:41 +0000

    fix: resolve merge conflict in Home component imports

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
 import '../theme/theme.css';
 
 import Navbar from '../components/Navbar';
-=======
-import "../theme/theme.css";
-
-import Navbar from "../components/Navbar";
->>>>>>> aebcd5895d5a917f0ed4d29c96afdeafb4839856
 
 export default function Home() {
   return <Navbar userName="Jorge" avatarUrl="/Ellipse.png" />;

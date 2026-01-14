dac4ab975adcb40c191ee0666105b5eca52146fa
Marta Silva <martacss@ua.pt>
2026-01-14 21:21:34 +0000

    Fix errors ig

--- frontend/src/layout/Layout.tsx
+++ frontend/src/layout/Layout.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import Navbar from '../components/Navbar';
-import { Sidebar } from '../components/Sidebar';
+import { Sidebar } from '../components/SideBar';
 
 export const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const [menuOpen, setMenuOpen] = useState(false);

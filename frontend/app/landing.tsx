51b34a96ec8891add3e5ab9dc0f46bd9e821d97b
Marta Silva <martacss@ua.pt>
2026-03-22 19:50:45 +0000

    login, register, loading and landing pages are completed.

--- frontend/app/landing.tsx
+++ frontend/app/landing.tsx
@@ -3,10 +3,42 @@ import styled from 'styled-components/native';
 import { ImageBackground } from 'react-native';
 import { StaticImages } from '../assets/images/landing';
 import Head from 'expo-router/head';
-
 import PrimaryButton from '../components/PrimaryButton';
 import SecondaryButton from '../components/SecondaryButton';
 import TertiaryButton from '../components/TertiaryButton';
+
+export default function Landing() {
+  return (
+    <Background source={StaticImages.landingBg} accessible={false}>
+      <Head>
+        <title>Welcome to Safinity!</title>
+      </Head>
+      <Stack.Screen options={{ title: 'Welcome to Safinity!', headerShown: false }} />
+
+      <WelcomeTitle accessibilityRole="header" accessibilityHeadingLevel={1}>
+        Welcome to Safinity!
+      </WelcomeTitle>
+
+      <Content accessibilityRole="main">
+        <PrimaryButton
+          accessibilityLabel="Log in"
+          title="Log in"
+          onPress={() => router.push('/login')}
+        />
+
+        <SecondaryButton
+          accessibilityLabel="Create account"
+          title="Create account"
+          onPress={() => router.push('/register')}
+        />
+
+        <TertiaryButton title="Other options" onPress={() => console.log('Other options')} />
+      </Content>
+    </Background>
+  );
+}
+
+// --------------------------------------------------------- Stuled Components ---------------------------------------------------------
 
 const Background = styled(ImageBackground).attrs({
   imageStyle: {
@@ -25,21 +57,10 @@ const Content = styled.View`
   margin-bottom: ${({ theme }) => theme.spacing.md}px;
 `;
 
-export default function Landing() {
-  return (
-    <Background source={StaticImages.landingBg} accessible={false}>
-      <Head>
-        <title>Welcome to Safinity!</title>
-      </Head>
-      <Stack.Screen options={{ title: 'Welcome to Safinity!', headerShown: false }} />
-
-      <Content>
-        <PrimaryButton title="Log in" onPress={() => router.push('/login')} />
-
-        <SecondaryButton title="Create account" onPress={() => router.push('/register')} />
-
-        <TertiaryButton title="Other options" onPress={() => console.log('Other options')} />
-      </Content>
-    </Background>
-  );
-}
+const WelcomeTitle = styled.Text`
+  color: ${({ theme }) => theme.colors.white};
+  font-size: 30px;
+  text-align: center;
+  font-weight: ${({ theme }) => theme.fonts.weights.extralight};
+  margin-bottom: ${({ theme }) => theme.spacing.xl}px;
+`;
